007d9ab1b26a677c25357e3bebc12cc040135d56
relly <rellycheng@163.com>
2020-04-26 15:01:04 +0800

    1

--- src/App.js
+++ src/App.js
@@ -9,25 +9,7 @@ export default function App() {
       <h2>nice too meet you.</h2>
 
       <div class="container" onselectstart="return false;" unselectable="on">
-        <div class="body_left">
-          <img src={biubiubiu} alt="" ondragstart="return false;" />
-        </div>
-
-        <div class="body_center love">
-          <div class="block">
-            <div class="div1" />
-            <div class="div2" />
-            <div class="div3" />
-            <div class="div4" />
-          </div>
-        </div>
-      </div>
-
-      <div class="footer">
-        <div class="border">
-          <div class="border-top" />
-          <div class="border-bottom" />
-        </div>
+        <img src={biubiubiu} alt="" ondragstart="return false;" />
       </div>
     </div>
   );
