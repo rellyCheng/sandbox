edebe5fa4d5c1a59de92d9588f7ea2a60dd23a52
relly <rellycheng@163.com>
2020-04-26 15:13:36 +0800

    1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import biubiubiu from "./img/biubiubiu.gif";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello qiuting</h1>
-      <h2>nice too meet you.</h2>
-
+      <h1>Hello</h1>
       <div class="container" onselectstart="return false;" unselectable="on">
         <img src={biubiubiu} alt="" ondragstart="return false;" />
       </div>
